f604994e9f94611f1d621328b1aa2bb2c3c7688a
unknown <ahmedmohammmed223@gmail.com>
2024-04-25 17:52:01 +0200

    Business_Logic

--- api/views.py
+++ api/views.py
@@ -1,13 +1,49 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets ,status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from . models import *
 from .serializers import *
-
+from django.contrib.auth.models import User
 # Create your views here.
 
 class MealViewSet(viewsets.ModelViewSet):
     serializer_class = MealSerializer
     queryset  = Meal.objects.all()
+    
+    @action(methods = ['POST'],detail = True)
+    def rate_meal(self,request,pk = None ):
+        if 'stars' in request.data:
+            '''create or update'''
+            meal = Meal.objects.get(pk=pk)
+            user = User.objects.get(username = request.data['username'])
+            stars = request.data['stars']
+            
+            try:
+                #update
+                rate = Rating.objects.get(user = user.id , meal = meal.id) #Specific rate
+                rate.stars = stars
+                rate.save()
+                serializer = RatingSerializer(rate,many = False)
+                json = {
+                    "message":'Meal Rate Updated',
+                    "result": serializer.data
+                }
+                return Response(json,status=status.HTTP_202_ACCEPTED)
+                
+            except:
+                #create
+                rate = Rating.objects.create(stars = stars , meal = meal , user = user)
+                serializer = RatingSerializer(rate,many = False)
+                json = {
+                    "message":'Meal Rate Created',
+                    "result": serializer.data
+                }
+                return Response(json,status=status.HTTP_201_CREATED)
+        else:    
+            return Response({"message":"Stars not Provided"},status= status.HTTP_400_BAD_REQUEST)
+        
+    
     
 class RatingViewSet(viewsets.ModelViewSet):
     queryset  = Rating.objects.all()
