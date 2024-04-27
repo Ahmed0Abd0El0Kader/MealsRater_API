47367e17124eb1deb5968600a945f8e681396aaf
unknown <ahmedmohammmed223@gmail.com>
2024-04-27 16:31:44 +0300

    Review_Deploy

--- mealrater/settings.py
+++ mealrater/settings.py
@@ -39,14 +39,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'api',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'https://example.com',
+    'https://sub.example.com',
+    'http://localhost:8080',
+    'http://127.0.0.1:8000',
+]
 
 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
